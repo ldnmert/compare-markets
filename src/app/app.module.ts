7bb62905c730b9f33344b499985aa7a5743921dd
mert <merteldener7@gmail.com>
2024-03-28 16:45:59 +0300

    firstcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    SideBarComponent,
+    SearchBarComponent,
+    HeaderComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    
   ],
   providers: [
     provideClientHydration()
